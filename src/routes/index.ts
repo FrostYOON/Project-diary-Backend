7668c11e7f3c80b4d23dd77ba1c02f7e177a0467
FrostYOON <yys1423@naver.com>
2025-01-24 23:26:18 +0900

    Fix: route > index 중복 수정

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authRoutes from "./auth.routes";
-
-const router = express.Router();
-
-// 인증 관련 라우트
-router.use("/auth", authRoutes);
-
-export default router;
